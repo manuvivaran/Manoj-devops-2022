fcaebee50bf383d149a7a6f3d4a32f2f94ee1b8c
Cloud User <ec2-user@ip-172-31-0-233.ec2.internal>
2022-05-28 19:50:23 +0000

    Jenkinfile 6

--- upnotes.sh
+++ upnotes.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 tstamp="$(date +'%d-%B-%y')"
 git add --all && git commit -m "$* $[tstamp]" && git push https://github.com/manuvivaran/Manoj-devops-2022.git
-Username for 'https://github.com': manuvivaran
-Password for 'https://manuvivaran@github.com':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 
 
